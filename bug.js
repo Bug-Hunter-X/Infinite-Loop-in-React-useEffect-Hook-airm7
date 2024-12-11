7ef98de2b5fca721b2415c3aa2a3573584934580
BugHunter-X <askzenai@gmail.com>
2024-12-11 14:20:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,21 @@
+```javascript
+import React, { useState, useEffect } from 'react';
+
+function MyComponent() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    // This effect will run infinitely because it doesn't have a dependency array
+    console.log('Effect running');
+    setCount(count + 1);
+  }, []);
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+    </div>
+  );
+}
+
+export default MyComponent;
+```
